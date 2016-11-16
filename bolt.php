cc117ced07a11a0285a38189c01ae3bf13248c97
a13x.jf <a13x.jf@gmail.com>
2016-11-17 00:34:41 +0100

    updated structure

--- bolt.php
+++ bolt.php
@@ -1,17 +1,16 @@
 <?php
-
 require_once 'vendor/autoload.php';
 require_once 'access.php';
+
+var_dump($user);
 
 use GraphAware\Neo4j\Client\ClientBuilder;
 
 $config = \GraphAware\Bolt\Configuration::newInstance()
-	    ->withCredentials($user, $pass)
-		    ->withTLSMode(\GraphAware\Bolt\Configuration::TLSMODE_REQUIRED);
+	->withCredentials($user_grh, $pass_grh)
+		->withTLSMode(\GraphAware\Bolt\Configuration::TLSMODE_REQUIRED);
 
-$driver = \GraphAware\Bolt\GraphDatabase::driver('bolt://hobby-onnhibppojekgbkekignbiol.dbs.graphenedb.com:24786', $config);
+$driver = \GraphAware\Bolt\GraphDatabase::driver("bolt://$url_grh:24786", $config);
 $session = $driver->session();
 
-
 var_dump($session);
-
--- test.php
+++ test.php
@@ -1,31 +1,41 @@
 <?php
-
 require_once 'vendor/autoload.php';
 require_once "access.php";
 
+echo "Query is MATCH (n:Person) RETURN n, n.name as name";
+echo "<pre>";
 
 use GraphAware\Neo4j\Client\ClientBuilder;
 
+$time_start = microtime(true);
+
 $client = ClientBuilder::create()
-		    ->addConnection('bolt', 'bolt://hobby-onnhibppojekgbkekignbiol.dbs.graphenedb.com:24786')->build();
+	->addConnection('default', "http://$user_grh:$pass_grh@$url_grh:24789") // Example for HTTP connection configuration (port is optional)                                
+		->addConnection('bolt', "bolt://$user_grh:$pass_grh@url_grh:24786") // Example for BOLT connection configuration (port is optional)                                    
+			->build();
 
-
-//$query = "MATCH (n:Person) RETURN n, n.name as name";
-$query = 'MATCH (m:Movie)<-[r:ACTED_IN]-(p:Person) RETURN m,r,p';
-$result = $client->run($query);
-
+$query = "MATCH (n:Person) RETURN n, n.name as name";
+//$query = 'MATCH (m:Movie)<-[r:ACTED_IN]-(p:Person) RETURN m,r,p';
+//$result = $client->run($query);
 foreach ($result->getRecords() as $record) {
-	var_dump($record);
- echo sprintf("Person name is : %s \n", $record->value('name'));
+//      var_dump($record); 
+echo sprintf("Person name is : %s \n", $record->value('name'));
 }
 
 
+$time_end = microtime(true);
+
+$execution_time = ($time_end - $time_start);
+
+echo '<b>Total Execution Time:</b> '.$execution_time.' sec';
+
 //$query = "MATCH (n:Person) RETURN n, n.name as name LIMIT 3";
+
 //$result = $client->run($query);
 
-//foreach ($result->getRecords() as $record) {
-//		var_dump($record);
-//	    echo ($record->value("name")), PHP_EOL;
+
+
+//foreach ($result->getRecords() as $record){
+//	var_dump($record);
+//	echo ($record->value("name")), PHP_EOL;
 //}
-
-
